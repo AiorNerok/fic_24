cc4c00fdf436b1c4e37743699b3a649d20d3f479
Nerok <90855034+AiorNerok@users.noreply.github.com>
2024-12-01 21:09:11 +0300

    edit dropdown menu

--- components/entities/profile/ui/profile.tsx
+++ components/entities/profile/ui/profile.tsx
@@ -17,6 +17,8 @@ import {
 import { SvgIcons } from "@/components/shared/ui/svg-icons"
 import { Button } from '@/components/shared/ui/button'
 import { signOut } from "next-auth/react"
+import Link from "next/link"
+import { CreditCard, LogOut, Settings2 } from "lucide-react"
 
 export const Profile = () => {
     return (
@@ -27,52 +29,27 @@ export const Profile = () => {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuLabel className="flex flex-row gap-2">
+                    <div>
+                        <SvgIcons.eynBig />
+                    </div>
+                    <div>
+                        Эйнам
+                        <Link className="block" href={'mailto:#'}> eynam@example.com</Link>
+                    </div>
+                </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        Profile
-                        <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    <DropdownMenuItem disabled>
+                        <Settings2 /> Настройки
                     </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        Billing
-                        <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        Settings
-                        <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        Keyboard shortcuts
-                        <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                    <DropdownMenuItem disabled>
+                        <CreditCard /> Кошелек
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                        <DropdownMenuPortal>
-                            <DropdownMenuSubContent>
-                                <DropdownMenuItem>Email</DropdownMenuItem>
-                                <DropdownMenuItem>Message</DropdownMenuItem>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>More...</DropdownMenuItem>
-                            </DropdownMenuSubContent>
-                        </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuItem>
-                        New Team
-                        <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>GitHub</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuItem disabled>API</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={()=>signOut()}>
-                    Sign Out
+                <DropdownMenuItem onClick={() => signOut()}>
+                    <LogOut /> Выход
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
